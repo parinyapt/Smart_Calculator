ca21bf4fe2918246854aa39e62210da0710ffb7e
parinyapt <parinya_team@hotmail.com>
2020-03-13 14:07:34 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const { Fcal } = require('fcal');
+
+const fcal = new Fcal();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'N/A' ,outputre: 'N/A' });
+});
+
+/*router.get('/:equation', function(req, res, next) {
+  var equation = String(req.params.equation)
+  var reout = "Not found result output"
+  if(equation==0){
+  
+  }
+  else{
+    reout = eval(equation)
+  }
+  res.render('index', { title: equation , resultoutput: reout });
+});*/
+router.post('/', function(req, res, next) {
+  var equation = "" 
+  equation = req.body.equation
+  var reout = "Not found result output"
+  if(equation==0){
+    equation = "N/A"
+    reout = "N/A"
+  }
+  else{
+    reout = fcal.evaluate(equation)
+  }
+  res.render('index', { title: equation ,outputre: reout });
+});
+
+module.exports = router;
